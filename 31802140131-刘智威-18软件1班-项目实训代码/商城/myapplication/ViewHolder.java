30cd07b0ccb0d472f4ab26209883c1638f4797cf
My-lzw <59674185+My-lzw@users.noreply.github.com>
2020-01-09 10:11:20 +0800

    Add files via upload

---
+++ 31802140131-刘智威-18软件1班-项目实训代码/商城/myapplication/ViewHolder.java
@@ -0,0 +1,14 @@
+package com.example.administrator.myapplication;
+
+import android.widget.ImageView;
+import android.widget.TextView;
+
+/**
+ * @创建者：CFJ
+ * @创建时间：${daate} ${time}
+ * @描述：Android应用开发
+ */
+class ViewHolder {
+    TextView title,price;
+    ImageView iv;
+}
